f2c8d602da179ee1402163529323c483cb51184d
Aliakbar Saleh bigdeli <al.ak.saleh@gmail.com>
2018-11-08 14:43:54 +0330

    first complete

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 #[derive(Debug)]
 struct Node {
     elem: i32,
@@ -10,15 +12,39 @@ enum Link {
     More(Box<Node>),
 }
 
+#[derive(Debug)]
 pub struct List {
     head: Link,
 }
 
-// impl List {
-//     pub fn new() -> List {
-//         List::Empty
-//     }
-// }
+impl List {
+    pub fn new() -> Self {
+        List{head: Link::Empty}
+    }
+}
+
+impl List {
+    fn push(&mut self, item: i32) {
+        let new_node = Box::new(Node {
+            elem: item,
+            next: mem::replace(&mut self.head, Link::Empty)
+        });
+    }
+}
+
+
+impl List {
+    fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+        Link::Empty => None,
+        Link::More(boxed_node) => {
+            let node = *boxed_node;
+            self.head = node.next;
+            Some(node.elem)
+            }
+        }
+    }
+}
 
 // impl List {
 //     pub fn add(&mut self, item: i32) -> List{
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ extern crate list;
 use list::first::List;
 
 fn main() {
-    
+    let l = List::new();
+    println!("{:?}", l);
 }                             
